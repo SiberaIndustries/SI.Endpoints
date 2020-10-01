9856157e7f201be86e9dbb1c19de76289fff5c4c
Tobias Sibera <discjoggy@gmx.de>
2020-10-01 22:36:37 +0200

    Fix warnings

--- SI.Endpoints/EndpointsConfiguration.cs
+++ SI.Endpoints/EndpointsConfiguration.cs
@@ -3,7 +3,9 @@
     public class EndpointsConfiguration
     {
         public string Prefix { get; private set; } = string.Empty;
+
         public bool FeaturesUsed { get; private set; }
+
         public bool EndpointNamesIgnored { get; private set; }
 
         public EndpointsConfiguration WithPrefix(string prefix)
