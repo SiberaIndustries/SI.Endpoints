9c502c768a3e6a3e62dd9d322012bd504921e5fe
Tobias Sibera <Tobias.Sibera@live.de>
2022-11-15 23:36:39 +0100

    Add spaces

--- SI.Endpoints.Sample/Endpoints/TodoItem/Get.GetTodoItemResponse.cs
+++ SI.Endpoints.Sample/Endpoints/TodoItem/Get.GetTodoItemResponse.cs
@@ -3,7 +3,9 @@
     public class GetTodoItemResponse
     {
         public int Id { get; set; }
+
         public string? Title { get; set; }
+
         public string? Description { get; set; }
     }
 }
