9dd8759d9d44fadf0b3fef2961343f0441e35992
Tobias Sibera <Tobias.Sibera@live.de>
2022-11-17 01:20:58 +0100

    Update List endpoint (use IAsyncEnumerable as an example, Rename TodoItemResponse to ListTodoItemResponse, Rename List to ListEndpoint)

---
+++ SI.Endpoints.Sample/Endpoints/TodoItem/List.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SI.Endpoints.Sample.Endpoints.TodoItem
+{
+    public class List : AsyncEnumerableEndpointWithResponse<ListTodoItemResponse>
+    {
+        [HttpGet]
+        public override ActionResult<IAsyncEnumerable<ListTodoItemResponse>> HandleAsync()
+        {
+            var todos = new[]
+            {
+                new ListTodoItemResponse
+                {
+                    Id = 1,
+                    Title = "Title A",
+                    Description = "Description A"
+                },
+                new ListTodoItemResponse
+                {
+                    Id = 2,
+                    Title = "Title B",
+                    Description = "Description B"
+                }
+            };
+
+            async IAsyncEnumerable<ListTodoItemResponse> todosAsync()
+            {
+                foreach (var todo in todos)
+                {
+                    yield return todo;
+                    await Task.Delay(1000);
+                }
+            }
+
+            return Ok(todosAsync());
+        }
+    }
+}
