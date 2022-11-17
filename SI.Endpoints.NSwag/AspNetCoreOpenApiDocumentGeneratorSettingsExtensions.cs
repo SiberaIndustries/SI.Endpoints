790a6c12b493779ede47e7372b6fb2cf483ac16a
Tobias Sibera <Tobias.Sibera@live.de>
2022-11-17 01:23:01 +0100

    Add NSwag support

---
+++ SI.Endpoints.NSwag/AspNetCoreOpenApiDocumentGeneratorSettingsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using NSwag.Generation.AspNetCore;
+
+namespace SI.Endpoints.NSwag
+{
+    public static class AspNetCoreOpenApiDocumentGeneratorSettingsExtensions
+    {
+        public static void EnableFeatureFilter(this AspNetCoreOpenApiDocumentGeneratorSettings settings, bool replaceExistingTags = true)
+        {
+            settings.OperationProcessors.Add(new FeatureFilter(replaceExistingTags));
+        }
+    }
+}
---
+++ SI.Endpoints.NSwag/FeatureFilter.cs
@@ -0,0 +1,41 @@
+﻿using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using SI.Endpoints.Core;
+using System.Linq;
+using System.Reflection;
+
+namespace SI.Endpoints.NSwag
+{
+    public class FeatureFilter : IOperationProcessor
+    {
+        private readonly bool replaceExistingTags = true;
+
+        public FeatureFilter()
+        {
+        }
+
+        public FeatureFilter(bool replaceExistingTags)
+        {
+            this.replaceExistingTags = replaceExistingTags;
+        }
+
+        public bool Process(OperationProcessorContext context)
+        {
+            if (context.MethodInfo.DeclaringType.IsSubclassOf(typeof(EndpointBase)) &&
+                (context.MethodInfo.Name == "HandleAsync" || context.MethodInfo.Name == "Handle") &&
+                FeatureResolver.TryResolve(context.MethodInfo.DeclaringType.GetTypeInfo(), out string? feature))
+            {
+                if (replaceExistingTags || context.OperationDescription.Operation.Tags == null)
+                {
+                    context.OperationDescription.Operation.Tags = new[] { feature }.ToList();
+                }
+                else
+                {
+                    context.OperationDescription.Operation.Tags.Add(feature);
+                }
+            }
+
+            return true;
+        }
+    }
+}
